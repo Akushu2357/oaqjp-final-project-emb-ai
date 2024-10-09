39e37f7f76dec0bbb70adaf6c196fb59229e66c1
worapat.chuc <worapat.chuc@kmutt.ac.th>
2024-10-09 10:02:51 -0400

    task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,6 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
-    return formatted_response
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    emotion['dominant_emotion'] = max(emotion, key= lambda x: emotion[x])
+    return emotion
